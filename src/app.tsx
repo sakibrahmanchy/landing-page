5246e347aedd26d49c9f45e6d154bf27bf3afa3b
Sakibur <sakibur@sixads.net>
2021-07-15 03:52:36 +0600

    Fixed several issues and modified readme

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./app.css";
 import Banner from "./components/banner.component";
 import Companies from "./components/company/companies.component";
 import JobOffers from "./components/job-offer/job-offers.component";
+import Loader from "./components/loader.component";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+      setLoading(false);
+  }, []);
   return (
     <div className="App">
-      <Banner />
-      <div className="p-4 md:p-20">
-        <JobOffers />
-        <Companies />
-      </div>
+      {loading && <Loader />}
+      {!loading && (
+        <div>
+          <Banner />
+          <div className="p-4 md:p-20">
+            <JobOffers />
+            <Companies />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
